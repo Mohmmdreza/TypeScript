9d25187f7100aa69d5ca892b1ae034917bdcd6bc
Mohamad Reza Tnh <m.reza.tnh@gmail.com>
2022-06-20 07:27:21 +0430

    work with objec type

--- app.js
+++ app.js
@@ -1,25 +1,2 @@
-// const button = document.querySelector("button") ;
-// const input1 = document.getElementById("num1")! as HTMLInputElement;
-// const input2 = document.getElementById("num2")! as HTMLInputElement;
-// function add(num1:number, num2:number) {
-//     return num1 + num2
-// }
-// button.addEventListener("click", function () {
-//   console.log(add(+input1.value, +input2.value))
-//  })
-function add(num1, num2, showResult, phrase) {
-    // if(typeof num1 !== "number" || typeof num2 !== "number"){
-    //     throw new Error("incorrect type!")
-    // }
-    var result = num1 + num2;
-    if (showResult) {
-        console.log(phrase + result);
-    }
-    else
-        return num1 + num2;
-}
-var number1 = 5;
-var number2 = 6;
-var printResult = true;
-var resultPhrase = "Phrase is: ";
-var result = add(number1, number2, printResult, resultPhrase);
+var person = { name: "Mamzi", age: '21' };
+console.log(person);
---
+++ basics.ts
@@ -0,0 +1,35 @@
+
+// const button = document.querySelector("button") ;
+// const input1 = document.getElementById("num1")! as HTMLInputElement;
+// const input2 = document.getElementById("num2")! as HTMLInputElement;
+
+// function add(num1:number, num2:number) {
+//     return num1 + num2
+// }
+
+// button.addEventListener("click", function () {
+//   console.log(add(+input1.value, +input2.value))
+//  })
+
+
+function add (num1:number , num2:number ,showResult:boolean , phrase:string) {
+    // if(typeof num1 !== "number" || typeof num2 !== "number"){
+    //     throw new Error("incorrect type!")
+    // }
+
+    let result = num1 + num2
+
+    if (showResult){
+        console.log(phrase + result)
+    }
+    else return num1 + num2;
+}
+
+let number1:number ; 
+number1 = 5;  
+const number2 = 6 ;
+const printResult =  true ; 
+const resultPhrase = "Phrase is: "
+
+const result = add(number1, number2 ,printResult ,resultPhrase) ;
+
