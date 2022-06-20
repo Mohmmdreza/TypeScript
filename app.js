956c08547de3efa9d4058f31dbc1867af6a85c6c
Mohamad Reza Tnh <m.reza.tnh@gmail.com>
2022-06-20 07:35:19 +0430

    work with array type

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
-var person = { name: "Mamzi", age: '21' };
+// const person:{
+//     name: string;
+//     age: number;
+// } = { name : "Mamzi" , age: 21}
+var person = {
+    name: "Mamzi", age: 21, hobbies: ["Sport", "Food"]
+};
+var favoriteActivities;
+favoriteActivities = ["Sport"];
+for (var _i = 0, _a = person.hobbies; _i < _a.length; _i++) {
+    var hobby = _a[_i];
+    console.log(hobby.toUpperCase());
+}
 console.log(person);
--- app.ts
+++ app.ts
@@ -1,6 +1,18 @@
-const person:{
-    name: string;
-    age: number;
-} = { name : "Mamzi" , age: 21}
+// const person:{
+//     name: string;
+//     age: number;
+// } = { name : "Mamzi" , age: 21}
+
+const person ={
+    name: "Mamzi" , age:21 , hobbies :["Sport" , "Food"]
+}
+
+let favoriteActivities:string[];
+
+favoriteActivities = ["Sport"];
+
+for ( let hobby of person.hobbies){
+    console.log(hobby.toUpperCase())
+}
 
 console.log(person)
