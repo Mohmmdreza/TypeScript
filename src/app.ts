1b18ee2b99d28fe38f5f22e99eeabca6ac337349
Mohamad Reza Tnh <m.reza.tnh@gmail.com>
2022-06-22 22:07:45 +0430

    work with let and var

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,24 @@
-const button = document.querySelector("button") !;
+// const button = document.querySelector("button") !;
 
-button.addEventListener("click",()=>{
-    console.log("Click")
-})
+// button.addEventListener("click",()=>{
+//     console.log("Click")
+// })
+
+var result;
+
+let age = 29;
+
+function add(a:number , b:number){
+
+    result = a+b;
+    return result;
+
+}
+
+console.log(result);    
+
+if(age > 20){
+    let isOld =true
+}
+
+console.log(isOld)
