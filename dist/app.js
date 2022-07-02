ab6b33578bb67cfd373a5b70a5192a8b21e5d7f5
Mohamad Reza Tnh <m.reza.tnh@gmail.com>
2022-07-02 11:34:58 +0430

    work with more on gurds

--- dist/app.js
+++ dist/app.js
@@ -1,1 +1,21 @@
 "use strict";
+const e1 = {
+    name: "Mamzi",
+    privilage: ["Programming"],
+    startDate: new Date
+};
+function add(a, b) {
+    if (typeof a === "string" || typeof b === "string") {
+        return a.toString() + b.toString();
+    }
+    return a + b;
+}
+function printEmployeeInformation(emp) {
+    if ('privilage' in emp) {
+        console.log("Privilage: " + emp.privilage);
+    }
+    if ('startDate' in emp) {
+        console.log("Start Date: " + emp.startDate);
+    }
+}
+printEmployeeInformation(e1);
--- src/app.ts
+++ src/app.ts
@@ -16,3 +16,32 @@ const e1:ElevatedEmployee ={
     startDate:new Date
 }
 
+
+type Combinable = string | number;
+type Numric = number | boolean;
+
+type Univeral = Combinable & Numric
+
+
+function add(a:Combinable, b:Combinable){
+
+if(typeof a === "string" || typeof b === "string"){
+   return a.toString() + b.toString()
+} 
+
+return a+b 
+
+}
+
+type UnknownEmployee = Employee & Admin
+
+function printEmployeeInformation(emp:UnknownEmployee){
+    if('privilage' in emp){
+        console.log("Privilage: " + emp.privilage)
+    }
+    if('startDate' in emp){
+        console.log("Start Date: " +emp.startDate)
+    }
+}
+
+printEmployeeInformation(e1)
